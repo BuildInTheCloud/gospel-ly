addc4cb66157821301a891cd1810f995c3eab0a7
Mark Hebert <mark@hasissues.com>
2016-11-12 20:48:34 -0500

    version 0.0.6 released to store

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Toast, ToastController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { Data } from '../../providers/data';
 
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html',
-  providers: [Data]
+  providers: []
 })
 
 export class SettingsPage {
   bibleVersion: string;
 
-  constructor(public navCtrl: NavController, public data: Data, public storage: Storage) {
+  constructor(public navCtrl: NavController, public data: Data, public storage: Storage, public toastCtrl: ToastController) {
     this.storage.get('bibleVersion').then(data => {
       this.bibleVersion = data ? data : "en_bbe";
     });
@@ -22,10 +22,20 @@ export class SettingsPage {
     if (this.bibleVersion != null) {
       this.storage.set('bibleVersion', this.bibleVersion)
       .then(
-        data => this.data.initLoadBible(),
+        data => {this.data.initLoadBible(); this.presentToast("Restart app for change to take effect");},
         error => console.log(error)
       );
     }
+  }
+
+  presentToast(message: string) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: "top"
+    });
+    toast.onDidDismiss(() => { });
+    toast.present();
   }
 
 }
